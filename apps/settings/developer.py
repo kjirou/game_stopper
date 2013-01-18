22d0b3e68b0bac2e592eb7909c666eea45e24b18
kjirou <kjirou.web@gmail.com>
2013-01-18 14:47:31 +0900

    Mod settings

--- apps/settings/developer.py
+++ apps/settings/developer.py
@@ -10,5 +10,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': PROJECT_DIR + '/game_stopper.sqlite',
-    }
+    },
+    #'default': {
+    #    'ENGINE': 'django.db.backends.mysql',
+    #    'NAME': 'booklistaid',
+    #    'USER': 'user',
+    #    'PASSWORD': 'password',
+    #    'HOST': 'localhost',
+    #    'PORT': '3306',
+    #},
 }
