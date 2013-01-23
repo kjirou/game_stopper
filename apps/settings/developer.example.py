e049aa97ec9e0228c659d13f125586ad803ac721
kjirou <kjirou.web@gmail.com>
2013-01-24 01:13:41 +0900

    Changed settings about database

--- apps/settings/developer.example.py
+++ apps/settings/developer.example.py
@@ -6,20 +6,5 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': PROJECT_DIR + '/game_stopper.sqlite',
-    },
-    #'default': {
-    #    'ENGINE': 'django.db.backends.mysql',
-    #    'NAME': 'booklistaid',
-    #    'USER': 'user',
-    #    'PASSWORD': 'password',
-    #    'HOST': 'localhost',
-    #    'PORT': '3306',
-    #},
-}
-
 TWAUTHORIZER_CONSUMER_KEY = 'Your key'
 TWAUTHORIZER_CONSUMER_SECRET = 'Your secret key'
