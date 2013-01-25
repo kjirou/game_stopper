6d7303c73934a13c5fd4cdd2eeca05035f86010d
kjirou <kjirou.web@gmail.com>
2013-01-26 03:24:31 +0900

    Shew saved-hours aggregation

--- apps/core/views.py
+++ apps/core/views.py
@@ -6,8 +6,13 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.template.loader import get_template
 from django.views.decorators.csrf import csrf_exempt
+from locks.models import Lock
 
 
 def index(request):
-    return render_to_response('core/index.html', {},
+    c = {
+        'total_saved_hours': Lock.objects.sum_saved_hours(),
+        'today_saved_hours': Lock.objects.sum_saved_hours(period=1)
+    }
+    return render_to_response('core/index.html', c,
         context_instance=RequestContext(request))
